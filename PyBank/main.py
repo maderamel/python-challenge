f7f0f82deeb84caec16803e91245ae31a6f27a5e
Maderamel <maderam96@gmail.com>
2023-02-20 21:26:52 -0800

    attempt to sum p/l columns

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,8 @@ import os
 import csv
 csvpath = os.path.join("./Resources/budget_data.csv")
 
+
+netPL = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     #print(next(csvreader)) #printed header row just to see if path worked and it did 
@@ -11,10 +13,16 @@ with open(csvpath) as csvfile:
     csv_header = next (csvfile)
 
 # calculate total number of months included in dataset
-    print(len(list(csvreader)))
+    print("Total Months: ", len(list(csvreader)))
 
 #calculate net total of profit/losses over entire perios
+    profitLoss = csvreader
+    for row in profitLoss:
+        if not str(row[1]).startswith ('P'):
+            profitLoss = profitLoss+ int(row[1])
+    print(profitLoss)
     
+
 
 # changes in profit/losses over entire period then average
 
